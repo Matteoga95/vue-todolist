36f8552912594b307dc3128512ca3664d30f9d07
matteoga95 <gallo.work95@gmail.com>
2022-10-28 15:51:48 +0200

    milestone 2

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,9 @@
 // MILESTONE 1
 // Stampare all'interno di una lista, un item per ogni todo. Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
 
+// MILESTONE 2
+// Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+
 
 const { createApp } = Vue;
 
@@ -13,22 +16,27 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-           tasks:[
-            {
-                text: 'Testo della task',
-                done: false
-            },
-            {
-                text: 'Testo della task 2',
-                done: true
-            },
-            {
-                text: 'Testo della task 3',
-                done: false
-            }
-           ]
+            tasks: [
+                {
+                    text: 'Testo della task',
+                    done: false
+                },
+                {
+                    text: 'Testo della task 2',
+                    done: true
+                },
+                {
+                    text: 'Testo della task 3',
+                    done: false
+                }
+            ]
         }
     },
-    methods: {}
-         
+    methods: {
+        done(i) {
+            //dobbiamo rimuovere l'elemento selezionato all'indice parametrizzato con lo splice
+            this.tasks.splice(i,1)
+        }
+    }
+
 }).mount('#app');
